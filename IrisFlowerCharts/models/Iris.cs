6e5eee621dbaca35ac42de1d42dd5d23283c87de
Alexander Kostarev <kostarev.a.p@gmail.com>
2020-10-25 19:48:48 +0300

    add iris model

---
+++ IrisFlowerCharts/models/Iris.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IrisFlowerCharts
+{
+    /// <summary>
+    /// Класс, представляющий Ирис Фишера
+    /// </summary>
+    class Iris
+    {
+        /// <summary>
+        /// Свойства Ириса
+        /// </summary>
+        public MathLib.MathVector Features { get; }
+
+        /// <summary>
+        /// Тип Ириса
+        /// </summary>
+        public System.String Type { get; }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="features">Вектор свойств Ириса</param>
+        /// <param name="type">Тип Ириса</param>
+        Iris(MathLib.MathVector features, System.String type)
+        {
+            Features = features;
+            Type = type;
+        }
+    }
+}
